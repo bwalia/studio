b58b8882a6ec375c50508f478ee8fb2520f6a948
GitLab Automation <automation@gitlab.local>
2025-09-16 11:25:24 +0100

    Logo renamed

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <Image src="/logo.png" alt="WorkStation Logo" width={300} height={79} className="text-primary" />
+      <Image src="/logo-no-bg.png" alt="WorkStation Logo" width={300} height={79} className="text-primary" />
       <span className="font-headline text-xl font-bold">WorkStation</span>
     </Link>
   );
