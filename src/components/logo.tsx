3b70666fc05afc5fcac27c1ebd724d4bcf7aec5c
Balinder Walia <balinder.walia@gmail.com>
2025-09-16 09:21:37 +0000

    Replace public/logo.png wih this logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export default function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2">
-      <Image src="/logo.png" alt="DevRel Hub Logo" width={24} height={24} className="text-primary" />
-      <span className="font-headline text-xl font-bold">DevRel Hub</span>
+      <Image src="/logo.png" alt="WorkStation Logo" width={24} height={24} className="text-primary" />
+      <span className="font-headline text-xl font-bold">WorkStation</span>
     </Link>
   );
 }
